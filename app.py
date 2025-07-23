7b0505a50e243c39d8bc1630ca064d18fea0019d
Maria Lokhandwala <marialokhandwala7861@gmail.com>
2025-07-23 19:19:56 +0530

    Added backend route to store data in MongoDB

--- app.py
+++ app.py
@@ -1,5 +1,12 @@
-from flask import render_template
+from pymongo import MongoClient
 
-@app.route('/')
-def todo_form():
-    return render_template('index.html')
+client = MongoClient("mongodb://localhost:27017/")
+db = client.todo_db
+collection = db.todos
+
+@app.route('/submittodoitem', methods=['POST'])
+def submit_item():
+    name = request.form['itemName']
+    desc = request.form['itemDescription']
+    collection.insert_one({"name": name, "description": desc})
+    return "Item submitted!"
